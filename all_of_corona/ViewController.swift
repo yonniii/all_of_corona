c7c3a9498fce99067086ae05695fc6a4f1816383
yonniii <rkdtjdus753@gmail.com>
2022-01-30 15:01:56 +0900

    Initial Commit

---
+++ all_of_corona/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  all_of_corona
+//
+//  Created by 강서연 on 2022/01/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
