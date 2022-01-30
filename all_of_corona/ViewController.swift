18d72eb42e96debede6d48d4c09765bfaaeaa3e3
yonniii <rkdtjdus753@gmail.com>
2022-01-30 15:27:45 +0900

    num up button

--- all_of_corona/ViewController.swift
+++ all_of_corona/ViewController.swift
@@ -9,6 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var upLabel: UILabel!
+    
+    @IBAction func upButton(_ sender: UIButton) {
+        num = num + 1
+        upLabel.text = String(num)
+    }
+    
+    var num: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
